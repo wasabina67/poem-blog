16eeffd5007746409353ddccd9cc2ded0c2e2ef1
wasabina67 <wasabina67@gmail.com>
2025-10-11 14:15:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import os
+from pathlib import Path
+from openai import OpenAI
+
+
+def tts(model: str, voice: str, text: str, output_file: str):
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    response = client.audio.speech.create(
+        model=model,
+        voice=voice,
+        input=text,
+    )
+
+    output_path = Path(output_file)
+    with open(output_path, "wb") as f:
+        f.write(response.content)
+
+
 def main():
     print("Hello from poem-blog!")
 
